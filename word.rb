0dc77d2a777051d1b19c551a391f24739e10a89a
brodiethiele <brodie.thiele@gmail.com>
2018-07-23 19:32:43 +0000

    automatically backed up by learn

--- word.rb
+++ word.rb
@@ -1,1 +1,1 @@
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub "A", "O"
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub "A", "O","a","o"
